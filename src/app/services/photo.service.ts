2724fd8e6d93d53e7972502059f90fa19544adda
Maria Alice <maria_alice_172@hotmail.com>
2022-04-20 22:20:22 -0300

    add camera and photo service

---
+++ src/app/services/photo.service.ts
@@ -0,0 +1,31 @@
+import { Injectable } from '@angular/core';
+import { Camera, CameraResultType, CameraSource, Photo } from '@capacitor/camera';
+
+@Injectable({
+  providedIn: 'root'
+})
+export class PhotoService {
+
+  public photos: UserPhoto[] = [];
+
+  constructor() { }
+
+  public async addNewToGallery() {
+    // Take a photo
+    const capturedPhoto = await Camera.getPhoto({
+      resultType: CameraResultType.Uri,
+      source: CameraSource.Camera,
+      quality: 100
+    });
+
+    this.photos.unshift({
+      filepath: 'aqui pode ser adicionado o path foto',
+      webviewPath: capturedPhoto.webPath
+    });
+  }
+}
+
+export interface UserPhoto {
+  filepath: string;
+  webviewPath: string;
+}
